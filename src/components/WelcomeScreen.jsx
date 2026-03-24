4f10bff332003c796d6d5e584263d6b272ba583d
theonwwhomakerules <business.3alaa@gmail.com>
2026-03-24 13:53:42 +0100

    Fix image source filename in WelcomeScreen component

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -37,7 +37,7 @@ const WelcomeScreen = ({ onContinue }) => {
       }} transition={{
         duration: 0.6
       }} className="mb-12">
-          <img src="/AlgeriePoste.svg.png" alt="شعار بريد الجزائر" className="w-48 h-auto" />
+          <img src="/AlgeriePostesvg.png" alt="شعار بريد الجزائر" className="w-48 h-auto" />
         </motion.div>
 
         <motion.div initial={{
